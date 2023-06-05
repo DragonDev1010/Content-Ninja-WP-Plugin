054d3a095d49dd1028c3dfeb7f90933cc2c83ab4
DragonDev1010 <provic0099@gmail.com>
2023-06-05 11:20:48 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name:  Content Ninja Plugin
+Plugin URI:   https://www.wpbeginner.com 
+Description:  A short little description of the plugin. It will be displayed on the Plugins page in WordPress admin area. 
+Version:      1.0
+Author:       Jackson
+Author URI:   https://www.wpbeginner.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpb-tutorial
+Domain Path:  /languages
+*/

9f95cf26c5d9d77bca5afbfb03aab9fa9a8dc9d0
DragonDev1010 <provic0099@gmail.com>
2023-06-15 11:37:51 -0400

    add menu and submenu pages

--- content-guardian.php
+++ content-guardian.php
@@ -37,41 +37,10 @@ add_action('add_meta_boxes', 'add_content_guardian_option');
 function render_content_guardian_metabox($post) {
   // Output the button with a label "Check Content"
   echo '<button type="button" id="editor-setting-check-content-button" class="button button-primary">Check Content</button>';
-
-  // Add JavaScript code to handle the button click event
-  ?>
-  <script type="text/javascript">
-      // document.addEventListener('DOMContentLoaded', function() {
-      //     // Handle the click event of the "Check Content" button
-      //     var checkContentButton = document.getElementById('check-content-button');
-      //     checkContentButton.addEventListener('click', function() {
-      //         // Get the content of the editor
-      //         var editorContent = wp.data.select('core/editor').getEditedPostContent();
-
-      //         // Make an AJAX request to send the content to the 3rd party API
-      //         var xhr = new XMLHttpRequest();
-      //         xhr.open('POST', 'http://localhost:3000/check-content', true);
-      //         xhr.setRequestHeader('Content-Type', 'application/json');
-      //         xhr.onreadystatechange = function() {
-      //             if (xhr.readyState === XMLHttpRequest.DONE) {
-      //                 if (xhr.status === 200) {
-      //                     // Handle the API response here
-      //                     console.log(xhr.responseText);
-      //                 } else {
-      //                     // Handle errors here
-      //                     console.error(xhr.statusText);
-      //                 }
-      //             }
-      //         };
-      //         xhr.send(JSON.stringify({ content: editorContent }));
-      //     });
-      // });
-  </script>
-  <?php
 }
 
 // Add "Content Guardian" menu item to the admin bar
-function add_content_guardian_menu_item($wp_admin_bar) {
+function add_content_guardian_menu_bar_item($wp_admin_bar) {
   // Add the main menu item with a dropdown
   $args = array(
       'id'    => 'content-guardian',
@@ -91,7 +60,7 @@ function add_content_guardian_menu_item($wp_admin_bar) {
   );
   $wp_admin_bar->add_node($args);
 }
-add_action('admin_bar_menu', 'add_content_guardian_menu_item', 999);
+add_action('admin_bar_menu', 'add_content_guardian_menu_bar_item', 999);
 
 function add_custom_js_to_post_page() {
   if (is_admin() && get_current_screen()->base === 'post') {
@@ -104,3 +73,117 @@ function add_custom_js_to_post_page() {
   }
 }
 add_action('admin_footer', 'add_custom_js_to_post_page');
+
+// Add the top-level menu page
+add_action('admin_menu', 'content_guardian_add_menu_page');
+function content_guardian_add_menu_page() {
+    global $submenu;
+    add_menu_page(
+        'Content Guardian',     // Page title
+        'Content Guardian',     // Menu title
+        'manage_options',       // Capability required to access the menu page
+        'content_guardian',     // Menu slug
+        '',   // Callback function to render the menu page
+        'dashicons-shield',     // Icon URL or dashicon class
+        2                       // Position in the menu
+    );
+}
+
+// Add the submenus
+add_action('admin_menu', 'content_guardian_add_submenus');
+function content_guardian_add_submenus() {
+    add_submenu_page(
+        'content_guardian',     // Parent menu slug
+        'Overview',             // Page title
+        'Overview',             // Menu title
+        'manage_options',       // Capability required to access the submenu page
+        'content_guardian_overview',    // Menu slug
+        'content_guardian_render_overview'   // Callback function to render the submenu page
+    );
+
+    add_submenu_page(
+        'content_guardian',
+        'Checker',
+        'Checker',
+        'manage_options',
+        'content_guardian_checker',
+        'content_guardian_render_checker'
+    );
+
+    add_submenu_page(
+        'content_guardian',
+        'History',
+        'History',
+        'manage_options',
+        'content_guardian_history',
+        'content_guardian_render_history'
+    );
+
+    add_submenu_page(
+        'content_guardian',
+        'Settings',
+        'Settings',
+        'manage_options',
+        'content_guardian_settings',
+        'content_guardian_render_settings'
+    );
+
+    remove_submenu_page('content_guardian', 'content_guardian');
+}
+
+// Render the submenus
+function content_guardian_render_overview() {
+    // Render the Overview page content here
+?>
+    <div class="wrap">
+        <h1>Content Guardian - Checker</h1>
+        <form method="post" action="">
+            <textarea name="content_checker" rows="5" cols="50"></textarea>
+            <br>
+            <input type="submit" name="submit" value="Submit">
+        </form>
+    </div>
+<?php    
+}
+
+function content_guardian_render_checker() {
+    // Render the Checker page content here
+}
+
+function content_guardian_render_history() {
+    // Render the History page content here
+}
+
+function content_guardian_render_settings() {
+    $post_types = get_post_types(array('public' => true), 'objects');
+?>
+    <div class="wrap">
+        <h1>Content Guardian - Settings</h1>
+        <form method="post" action="">
+            <?php
+            foreach ($post_types as $post_type) {
+            ?>
+                <label>
+                    <input 
+                        type="checkbox" 
+                        name="post_type[]" 
+                        value="<?php echo esc_attr($post_type->name); ?>" 
+                        <?php 
+                            // checked(in_array($post_type->name, $selected_post_types), true); 
+                        ?> 
+                        <?php 
+                            echo current_user_can('manage_options') ? '' : 'disabled'; 
+                        ?>
+                    >
+                    <?php echo esc_html($post_type->label); ?>
+                </label><br>
+            <?php
+            }
+            ?>
+            <br>
+            <input type="submit" name="submit" value="Save Changes">
+        </form>
+    </div>
+<?php    
+}
+

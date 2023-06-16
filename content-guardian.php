5628213b7832253a883ac805aae2f200264f4bee
DragonDev1010 <provic0099@gmail.com>
2023-06-15 20:48:56 -0400

    add wp_options

--- content-guardian.php
+++ content-guardian.php
@@ -132,8 +132,8 @@ function content_guardian_add_submenus() {
 }
 
 // Render the submenus
-function content_guardian_render_overview() {
-    // Render the Overview page content here
+function content_guardian_render_overview() {}
+function content_guardian_render_checker() {
 ?>
     <div class="wrap">
         <h1>Content Guardian - Checker</h1>
@@ -146,42 +146,44 @@ function content_guardian_render_overview() {
 <?php    
 }
 
-function content_guardian_render_checker() {
-    // Render the Checker page content here
-}
-
-function content_guardian_render_history() {
-    // Render the History page content here
-}
+function content_guardian_render_history() {}
 
 function content_guardian_render_settings() {
+    // Retrieve the saved post type options from wp_options
+    $saved_post_types = get_option('my_plugin_post_types', array());
+
+    // Handle form submission
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        // Get the checked post types from the submitted form
+        $checked_post_types = isset($_POST['post_types']) ? $_POST['post_types'] : array();
+
+        // Save the checked post types to wp_options
+        update_option('my_plugin_post_types', $checked_post_types);
+
+        echo 'Settings saved successfully!';
+    }
+    
     $post_types = get_post_types(array('public' => true), 'objects');
 ?>
     <div class="wrap">
         <h1>Content Guardian - Settings</h1>
         <form method="post" action="">
-            <?php
-            foreach ($post_types as $post_type) {
-            ?>
+            <?php wp_nonce_field('my_plugin_post_types_nonce', 'my_plugin_post_types_nonce'); ?>
+            <h2>Select post types:</h2>
+            <?php foreach ($post_types as $post_type) : ?>
                 <label>
                     <input 
                         type="checkbox" 
-                        name="post_type[]" 
+                        name="post_types[]" 
                         value="<?php echo esc_attr($post_type->name); ?>" 
-                        <?php 
-                            // checked(in_array($post_type->name, $selected_post_types), true); 
-                        ?> 
-                        <?php 
-                            echo current_user_can('manage_options') ? '' : 'disabled'; 
-                        ?>
+                        <?php checked(in_array($post_type->name, $saved_post_types)); ?>
                     >
                     <?php echo esc_html($post_type->label); ?>
-                </label><br>
-            <?php
-            }
-            ?>
+                </label>
+                <br>
+            <?php endforeach; ?>
             <br>
-            <input type="submit" name="submit" value="Save Changes">
+            <input type="submit" class="button button-primary" value="Save Settings">
         </form>
     </div>
 <?php    
